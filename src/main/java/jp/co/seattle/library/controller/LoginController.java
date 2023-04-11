da226ab45ec40274e6035131c33def09201edde5
tachibanaihikari <tachibanaihikari@localhost>
2023-04-11 19:41:26 +0900

    task2_修正

--- src/main/java/jp/co/seattle/library/controller/LoginController.java
+++ src/main/java/jp/co/seattle/library/controller/LoginController.java
@@ -46,10 +46,12 @@ public class LoginController {
 		// ユーザーが存在すればログイン、存在しなければエラー(タスク２)
 
 		if (Objects.isNull(selectedUserInfo)) {
-
+            model.addAttribute("errorMessage", "メールアドレスとパスワードが一致しません。");
 			return "login";
-		} else
-			model.addAttribute("errorMessage", "パスワードが一致しません。");
-		return "redirect:/home";
+		} 
+		else {
+			
+			return "redirect:/home";
+		}
 	}
 }
