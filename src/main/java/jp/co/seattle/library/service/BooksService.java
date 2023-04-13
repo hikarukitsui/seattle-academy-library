07d1ff040265d7d18d96dd4285036aa38b39449b
tachibanaihikari <tachibanaihikari@localhost>
2023-04-13 11:28:08 +0900

    task3/第２次コミット

--- src/main/java/jp/co/seattle/library/service/BooksService.java
+++ src/main/java/jp/co/seattle/library/service/BooksService.java
@@ -30,7 +30,7 @@ public class BooksService {
 	 * @return 書籍リスト
 	 */
 	public List<BookInfo> getBookList() {
-
+		
 		// TODO 書籍名の昇順で書籍情報を取得するようにSQLを修正（タスク３）
 		List<BookInfo> getedBookList = jdbcTemplate.query(
 				"SELECT * FROM books ORDER BY id, title, author, publisher, publish_date, isbn, description, thumbnail_url, thumbnail_name ASC",
